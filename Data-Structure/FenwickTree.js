392d170d4a7660e6fa0982a77a06fb4bc994e9e3
Atake-MKU <134023428+Atake-MKU@users.noreply.github.com>
2025-02-08 23:36:14 +0900

    Update FenwickTree.js

--- Data-Structure/FenwickTree.js
+++ Data-Structure/FenwickTree.js
@@ -1,22 +1,30 @@
 class FenwickTree {
     constructor(arr) {
-        this.tree = new Array(arr.length + 1).fill(0);
-        for (let i = 0; i < arr.length; i++) this.add(i + 1, arr[i]);
+        this.n = arr.length;
+        this.tree = new Array(this.n + 1).fill(0);
+        for (let i = 0; i < this.n; i++) this.add(i + 1, arr[i]); // 1-based index
     }
+
+    // i 番目に x を加算
     add(i, x) {
-        let bit = 1;
-        while (i < this.tree.length) {
-            if (bit & i) this.tree[i] += x, i += bit;
-            bit <<= 1;
+        while (i <= this.n) {
+            this.tree[i] += x;
+            i += (i & -i); // LSB を加算
         }
     }
-    get(i) {
+
+    // 1 から i までの区間和を取得
+    sum(i) {
         let rslt = 0;
-        let bit = 1;
-        while (i) {
-            if (bit & i) rslt += this.tree[i], i -= bit;
-            bit <<= 1;
+        while (i > 0) {
+            rslt += this.tree[i];
+            i -= (i & -i); // LSB を減算
         }
         return rslt;
+    }
+
+    // [l, r] の区間和を取得
+    rangeSum(l, r) {
+        return this.sum(r) - this.sum(l - 1);
     }
 }
